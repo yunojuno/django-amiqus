729125e3361a0fa0da52d750ff4c4e8ae359f334
Sam Holt <sam.holt@yunojuno.com>
2025-05-15 15:39:40 +0100

    Add tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,7 +2,7 @@ from unittest import mock
 
 from django.test import TestCase
 
-from amiqus.api import ApiError, _headers, _respond, _url, get, post
+from amiqus.api import ApiError, _headers, _respond, _url, get, post, patch
 from amiqus.settings import DEFAULT_REQUESTS_TIMEOUT
 
 
@@ -53,6 +53,23 @@ class ApiTests(TestCase):
         self.assertEqual(post("/", data=data), response.json.return_value)
         mock_post.assert_called_once_with(
             _url("/", 1),
+            headers=headers,
+            json=data,
+            timeout=DEFAULT_REQUESTS_TIMEOUT,
+        )
+
+    @mock.patch("requests.patch")
+    @mock.patch("amiqus.api._headers")
+    def test_patch(self, mock_headers, mock_patch):
+        """Test the patch function calls API correctly."""
+        response = mock.Mock()
+        response.status_code = 200
+        headers = mock_headers.return_value
+        data = {"foo": "bar"}
+        mock_patch.return_value = response
+        self.assertEqual(patch("/", data=data), response.json.return_value)
+        mock_patch.assert_called_once_with(
+            _url("/", 2),  # Note: Using API version 2 as specified in the function
             headers=headers,
             json=data,
             timeout=DEFAULT_REQUESTS_TIMEOUT,
